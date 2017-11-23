857570ead25f62b30aa5b5c367547f69f199b2d6
Niclas Niederdrenk <niclas1511@posteo.de>
2017-11-23 15:50:10 +0100

    ...

--- The2DGame/SpaceGame.cs
+++ The2DGame/SpaceGame.cs
@@ -50,7 +50,7 @@ namespace Game2D
             player = new Player(playerPos, playerTexture, playertTextureScale, playertTextureScale, playerTextureCoward);
             //Background
             backgroundTile = Content.Load<Texture2D>("spaceTile");
-            earthTexture = Content.Load<Texture2D>("doener2");
+            earthTexture = Content.Load<Texture2D>("earth");
             //Font
             Font = Content.Load<SpriteFont>("Courier New");
         }
@@ -71,7 +71,7 @@ namespace Game2D
                 if (gameObjects.Count == 0 || gameObjects[gameObjects.Count - 1].Position.X < 600)
                 {
                     var startPosition = new Vector2(GraphicsDevice.Viewport.Width, possibleY[random.Next(3)]);//random.Next(GraphicsDevice.Viewport.Height) - 
-                    gameObjects.Add(new Earth(startPosition, earthTexture, 2f, 5f));
+                    gameObjects.Add(new Earth(startPosition, earthTexture, 0.25f, 5f));
                 }
             }
             //Alles Updaten:
